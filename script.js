a880910c6a2ca132bc54a7959748472e6af6d7f5
Sebastian Rodriguez <sebastianrodrbrens@gmail.com>
2024-06-27 15:59:44 -0600

    Add logic of the game

--- script.js
+++ script.js
@@ -3,13 +3,13 @@ function getComputerChoice() {
 
     switch (choice) {
         case 1:
-            return "Rock";
+            return "rock";
 
         case 2:
-            return "Paper";
+            return "paper";
 
         case 3:
-            return "Scissors";
+            return "scissors";
     }
 }
 
@@ -18,10 +18,38 @@ function getHumanChoice() {
 }
 
 
-function whoWon(computer, human) {
-    
+function whoWonRound(computer, human) {
+    human = human.toLowerCase();
+    if (computer === "rock" && human === "rock" || computer === "paper" && human === "paper" || computer === "scissors" && human === "scissors") {
+        return "Tie! Choose again!"
+    } else if (computer === "rock" && human === "scissors" || computer === "paper" && human === "rock" || computer === "scissors" && human === "paper") {
+        computerScore++;
+        return "You Lose! Computer +1 point"
+    } else if (computer === "rock" && human === "paper" || computer === "paper" && human === "scissors" || computer === "scissors" && human === "rock") {
+        humanScore++;
+        return "You Won! Human +1 point"
+    } else {
+        return "What did you wrote!??"
+    }
 }
 
-// let computerChoice = getComputerChoice();
-// let humanChoice = getHumanChoice();
-// console.log(computerChoice + humanChoice);
+function playGame() {
+    for (let index = 1; index <= 5; index++) {
+        let computerChoice = getComputerChoice();
+        let humanChoice = getHumanChoice();
+        console.log(whoWonRound(computerChoice, humanChoice)); 
+    }
+    if (humanScore > computerScore) {
+        console.log("You Won the Game! Congrats!");
+    } else {
+        console.log("You lost! Try Again!");
+    }
+}
+
+
+
+
+let humanScore = 0;
+let computerScore = 0;
+
+playGame();
